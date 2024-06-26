2f81b8f77b585d013f0b780505119755674da302
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 15:35:39 +0100

    Password

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,28 @@
+class Password {
+  String password = "";
+
+  bool isValid() {
+    if (password.length < 8 || password.length > 16) {
+      return false;
+    }
+
+    if (!password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+
+    if (!password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+
+    if (!password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $password";
+  }
+}
