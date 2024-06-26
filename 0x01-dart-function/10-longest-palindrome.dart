f6d3572cd8590e481885bc2f7212f20154177b62
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 15:11:49 +0100

    Longest Palindrome

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,6 +1,10 @@
 import '9-palindrome.dart';
 
 String longestPalindrome(String s) {
+  if(s.length < 3) {
+    return "none"
+  }
+
   if (isPalindrome(s)) {
     return s;
   }
@@ -14,5 +18,5 @@ String longestPalindrome(String s) {
     continue;
   }
 
-  return none;
+  return "none";
 }
