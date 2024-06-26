bd463f1e07fadcb5d5dc56e9ae647f7c90a35190
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 17:21:27 +0100

    Inheritance

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,41 @@
+import "6-password.dart";
+
+class User extends Password {
+  int id;
+  String name;
+  int age;
+  double height;
+  String user_password;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.age,
+      required this.height,
+      required this.user_password})
+      : super(password: user_password);
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "age": age,
+      "height": height,
+      "password": password
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return new User(
+        id: userJson["id"],
+        name: userJson["name"],
+        age: userJson["age"],
+        height: userJson["height"],
+        user_password: userJson["user_password"]);
+  }
+
+  @override
+  String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${super.isValid()})";
+  }
+}
---
+++ 0x02-dart-classes/6-password.dart
@@ -0,0 +1,36 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  String get password => _password;
+
+  set password(String pass) {
+    _password = pass;
+  }
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
