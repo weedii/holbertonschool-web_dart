a23549a223b35b30839f54fdec89bd583cbef1e8
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 22:22:51 +0100

    Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,25 @@
+import "4-util.dart":
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    String res = await fetchUserData();
+    var decodedUser = jsonDecode(res);
+    String userID = decodedUser["id"];
+
+    String resOrders = await fetchUserOrders(userID);
+    List userOrders = jsonDecode(resOrders);
+
+    double totalPrice = 0;
+
+    for (var product in userOrders) {
+      String resPrice = await fetchProductPrice(product);
+      double productPrice = jsonDecode(resPrice);
+      totalPrice += productPrice;
+    }
+
+    return totalPrice;
+  } catch (err) {
+    return -1;
+  }
+}
