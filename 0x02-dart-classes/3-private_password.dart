3fa5f1f3b634703be3f88f3c70031280f18b2bdd
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 16:18:17 +0100

    Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,30 @@
+class Password {
+  String _password;
+
+  Password({required this.password}):_password=password;
+
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[A-Z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[a-z]'))) {
+      return false;
+    }
+
+    if (!_password.contains(RegExp(r'[0-9]'))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $_password";
+  }
+}
