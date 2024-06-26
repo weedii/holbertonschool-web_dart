b08d6edd4a0e106f8e7a237f579a6cb3d1c6be58
weedii <weedi@DESKTOP-2SCQG2R>
2024-06-26 17:05:19 +0100

    Json to User

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -25,6 +25,6 @@ class User {
 
   @override
   String toString() {
-    return "User(id: $id ,name: $name,  age: $age, height: $height)";
+    return "User(id : $id ,name: $name, age: $age, height: height)";
   }
 }
